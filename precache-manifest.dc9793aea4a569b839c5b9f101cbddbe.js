02f6c55c70023942ab3e25e092d700e6fad5d23e
macas5 <mkalinauskas1993@gmail.com>
2020-08-25 07:35:39 -0700

    Updates

---
+++ precache-manifest.dc9793aea4a569b839c5b9f101cbddbe.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d298b23a4b2c01a79de7df3865c358c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c640ff36ccf3af74772b",
+    "url": "/static/css/2.3e7f680e.chunk.css"
+  },
+  {
+    "revision": "997ba13150c4aec00ecc",
+    "url": "/static/css/main.2872846f.chunk.css"
+  },
+  {
+    "revision": "c640ff36ccf3af74772b",
+    "url": "/static/js/2.28fc8074.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.28fc8074.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "997ba13150c4aec00ecc",
+    "url": "/static/js/main.0f4a2289.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "94acdeffafb31127b27cb5eccc7dc888",
+    "url": "/static/media/desk-illustration.94acdeff.svg"
+  }
+]);
