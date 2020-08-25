e0119f881bdd8b11ad33c1fffc25c911f8d0ab78
macas5 <mkalinauskas1993@gmail.com>
2020-08-25 09:13:05 -0700

    Minor fixes

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,15 +5,15 @@ import './About.css'
 const About = () => {
   return (
     <div id='about' className='flex flex-wrap'>
-      <article className='w100 w-40-l center shadow-1 ma3 tl'>
+      <article className='w100 w32-l center shadow-1 ma3 tl'>
         <h2 className='tc'>Who am I?</h2>
         <p className='ma3'>I am Lithuanian developer who currently lives in UK.</p>
         <p className='ma3'>I am excited to build things which has potential to reach people across the world
         and I believe the time to make such things is best there ever was.</p>
       </article>
-      <article className='w100 w-40-l center shadow-1 ma3 tl flex flex-wrap'>
-        <h2 className='center'>What can I do?</h2>
-        <p className='ma3'>My knowledge includes these languages and frameworks:</p>
+      <article className='w100 w32-l center shadow-1 ma3 tl flex flex-wrap'>
+        <h2 className='tc basis-100'>What can I do?</h2>
+        <p className='ma3 basis-100'>My knowledge includes these languages and frameworks:</p>
         <div className='flex flex-wrap'>
           <ul className='mr5'>
             <li>HTML</li>
