084bd7a5e982db03f2c6cc4a48508a2ce07bf331
macas5 <mkalinauskas1993@gmail.com>
2020-09-02 17:41:15 +0100

    Updates

---
+++ precache-manifest.b25427beed33ae3f0a9e0748cb0cd8a2.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6af6985e4b864f20bb315151f71a05ba",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "1dbe464a854b2f1eaa7b",
+    "url": "/portfolio/static/css/2.21b012b8.chunk.css"
+  },
+  {
+    "revision": "a947b0003407a14753d0",
+    "url": "/portfolio/static/css/main.4a7570fc.chunk.css"
+  },
+  {
+    "revision": "1dbe464a854b2f1eaa7b",
+    "url": "/portfolio/static/js/2.cf7bfb18.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.cf7bfb18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a947b0003407a14753d0",
+    "url": "/portfolio/static/js/main.c27d61dc.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "f81172011b0b0fd8f95db12e196458f6",
+    "url": "/portfolio/static/media/Flag_of_Lithuania.f8117201.svg"
+  },
+  {
+    "revision": "da08e077435d424937ee4c60ec87c261",
+    "url": "/portfolio/static/media/Flag_of_the_United_Kingdom.da08e077.svg"
+  },
+  {
+    "revision": "32f0e9a910bd68d4450adf9ba9e2369c",
+    "url": "/portfolio/static/media/SmartBrain.32f0e9a9.png"
+  },
+  {
+    "revision": "7b614d444b6a34495d54a7d487c04b1c",
+    "url": "/portfolio/static/media/TestProject.7b614d44.png"
+  },
+  {
+    "revision": "0b6ac181dd105a17f3829e657cdaf96d",
+    "url": "/portfolio/static/media/ThisSite.0b6ac181.png"
+  },
+  {
+    "revision": "94acdeffafb31127b27cb5eccc7dc888",
+    "url": "/portfolio/static/media/desk-illustration.94acdeff.svg"
+  },
+  {
+    "revision": "2c1a7560c88ea83e6b2593cd07af8ad8",
+    "url": "/portfolio/static/media/gmail.2c1a7560.png"
+  }
+]);
